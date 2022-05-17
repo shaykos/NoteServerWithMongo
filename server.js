121a5eb9b3a4870398a216e43c00fc0d536cce77
Up2App <up2app4u@gmail.com>
2022-05-17 15:39:21 +0300

    add index html file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 5008;
 
 //temp require
@@ -8,12 +11,18 @@ const Note = require('./Models/NoteModel');
 
 //create the server instance
 let server = express();
+server.use(cors());
 server.use(express.json());
+server.use(express.static(path.join(__dirname, '/build')));
 
 //connect to db
 mongoose.connect(process.env.DB_URL, () => { console.log('connected to mongodb atlas server') });
 
 server.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname,'/build','index.html'));
+})
+
+server.post('/insert', async (req, res) => {
     try {
         //let note = new Note({title:"title"});
         //let savedNote = await Note.save(note);
@@ -25,6 +34,7 @@ server.get('/', async (req, res) => {
         res.status(500).json({error});
     }
 });
+
 
 //listen on port 5000
 server.listen(PORT, () => console.log(`listening on: http://localhost:${PORT}`));
