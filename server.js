b855980f61f0e4ad59891280475c331b03d23c96
Up2App <up2app4u@gmail.com>
2022-05-29 08:58:57 +0300

    init react website admin

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const PORT = process.env.PORT || 5008;
 const server = express();
 server.use(cors()); //allow us to access the server from each endpoint
 server.use(express.json()); //add json support for POST, GET, PUT, DELETE 
-server.use(express.static(path.join(__dirname, 'build/')));
+server.use(express.static(path.join(__dirname, 'client/build/')));
 
 //routes
 server.use('/api/notes', require('./controllers/noteController'));
 
 //Global Get Request
 server.get('*/*', async (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build/', 'index.html'));
 });
 
 //Run the server
-server.listen(PORT, ()=>console.log(`http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
